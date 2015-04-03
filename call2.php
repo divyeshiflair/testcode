619c7199cd1c05774c456aaa43650cb2d8670246
Divyesh Patoriya <divyesh.patoriya@iflair.com>
2015-04-03 19:01:19 +0530

    Dst commit

---
+++ call2.php
@@ -0,0 +1,4 @@
+
+<h2>This is Page Two content</h2>
+<p>This is a Page Two paragraph.</p>
+<p id="test">This is another paragraph.</p>
---
+++ quote/index.php
@@ -0,0 +1,25 @@
+<?php
+//ini_set('display_errors', '0');
+echo "<pre>";
+	print_r($_REQUEST);
+echo "</pre>";
+?>
+<?php
+/*
+	
+	RewriteEngine on
+	#RewriteBase    /vinix/
+	#ErrorDocument    /vinix/
+
+	Options All -Indexes
+	RewriteCond %{REQUEST_FILENAME} !-f
+	RewriteCond %{REQUEST_FILENAME} !-d
+
+	RewriteRule ^nl/([^/]*)$ index.php?files=$1	
+	RewriteRule ^nl/([^/]*)/([^/]*)/([^/]*)$ index.php?files=$1&catname=$2&catid=$3
+	RewriteRule ^nl/([^/]*)/([^/]*)$ index.php?files=$1&subpage=$2
+	RewriteRule ^nl/([^/]*)/([^/]*)/([^/]*)/([^/]*)/([^/]*)/([^/]*)$ index.php?files=$1&newsname=$5&newsid=$6
+
+
+*/
+?>
